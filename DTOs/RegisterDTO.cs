f20fa4545d1c5229443f4c5f8d0b20d8336ed17a
an <108365444+amxrac@users.noreply.github.com>
2025-03-10 09:24:55 +0100

    add registerdto

---
+++ DTOs/RegisterDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invoice_and_Payment_System.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords don't match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
